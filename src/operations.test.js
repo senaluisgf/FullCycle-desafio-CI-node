de4fc594b29c3dcb6d30620d9baf918b00b0051a
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2024-09-04 11:20:31 -0400

    test: :white_check_mark: add unit test to times operation

--- src/operations.test.js
+++ src/operations.test.js
@@ -1,4 +1,4 @@
-const {sum, sub, div} = require('./operations')
+const {sum, sub, div, times} = require('./operations')
 
 test("add 1 + 2 to be equal 3", () => {
   expect(sum(1, 2)).toBe(3)
@@ -8,6 +8,10 @@ test('sub 2 - 1 to be equal 1', () => {
   expect(sub(2, 1)).toBe(1)
 })
 
-test('sub 2 / 1 to be equal 2', () => {
+test('div 2 / 1 to be equal 2', () => {
   expect(div(2, 1)).toBe(2)
+})
+
+test('multiple 2 * 1 to be equal 2', () => {
+  expect(times(2, 1)).toBe(2)
 })
